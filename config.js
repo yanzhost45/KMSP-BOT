84ec518dc6c6f6e56856bfb54e4eea6ac92e54d8
SonzaiEkkusu <abhiedthea@gmail.com>
2025-04-19 02:23:49 +0700

    final result

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   SELLER_CODE: 'SELLER_KODE_KMSP',
   OWNER_NAME: 'Sonzai X シ',
   OWNER_USERNAME: 'November2k',
-  OWNER_ID: '679008122',
+  OWNER_ID: 'OWNER_ID',
   MONGO_DB: 'MONGO_DB_URL',
   DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
 };
