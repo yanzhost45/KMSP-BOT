3cb570ecd949c9a474e8686bd99a8f7b405d0e8d
yanzhost45 <yanzhost45@gmail.com>
2025-07-30 21:10:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   TOKEN: 'BOT_TOKEN',
-  SELLER_CODE: 'SELLER_KODE_KMSP',
-  OWNER_NAME: 'Sonzai X シ',
-  OWNER_USERNAME: 'November2k',
-  OWNER_ID: 'OWNER_ID',
+  SELLER_CODE: 'c79f6ad9a2f71e8b24d8ffce4627544f',
+  OWNER_NAME: 'YANZ X6531B',
+  OWNER_USERNAME: 'yann_store_rd',
+  OWNER_ID: '6677107475',
   MONGO_DB: 'MONGO_DB_URL',
-  DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
+  DISCLAIMER_LINK: 'https://t.me/yanzstoressh'
 };
